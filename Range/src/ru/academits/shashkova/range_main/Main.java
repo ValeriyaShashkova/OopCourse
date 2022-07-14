c2d15a94af9fc5c8722917707870ccad8848996a
ValeriyaShashkova <lerich9@gmail.com>
2022-07-14 14:49:21 +0300

    first commit

--- Range/src/ru/academits/shashkova/range_main/Main.java
+++ Range/src/ru/academits/shashkova/range_main/Main.java
@@ -2,6 +2,6 @@ package ru.academits.shashkova.range_main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world! 1");
     }
 }
